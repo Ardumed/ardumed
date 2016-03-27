0d9f806e545b0c2fb231ce499be9343fb2f91826
vatsalya25 <vatsalya25@gmail.com>
2016-03-27 18:32:30 +0530

    added path in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/src/login/login.html'));
