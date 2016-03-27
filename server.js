596a7e4a81f0cda92033b97d1889d643117dd5c8
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-03-27 18:15:45 +0530

    Added middleware for bower componenets

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + '/bower_components'));
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/app/html/index.html'));
 });
