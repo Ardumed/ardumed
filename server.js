03acd4ceb0a0081fd740be4b72650200f67ec586
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-03-29 00:50:27 +0530

    works well

--- server.js
+++ server.js
@@ -1,20 +1,75 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/src/assets'));
 app.use(express.static(__dirname + '/src/include'));
 app.use(express.static(__dirname + '/src/dashboard'));
-app.use(express.static(__dirname + '/src/login'));
+app.use(express.static(__dirname + '/src/assets'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/src/login/login.html'));
 });
 
+
 app.get('/dashboard', function (req, res) {
   res.sendFile(path.join(__dirname + '/src/dashboard/DashboardForm.html'));
 });
+
+
+app.post('/setprescription', function (req, res) {
+  getDetails(req);
+  res.sendFile(path.join(__dirname + '/app/html/setprescription.html'));
+});
+
+var getDetails = function(req){
+  var det = {};
+  det.name = req.body.patientName;
+  det.age = req.body.patientAge;
+  det.medNum = req.body.medicineNumber;
+  var current = new Date();
+  det.from = current;
+  current.setDate(current.getDay()+30);
+  det.to = current;
+  det.med1.morning = req.body.morningCheckbox1;
+  det.med1.noon = req.body.noonCheckbox1;
+  det.med1.night = req.body.nightCheckbox1;
+  det.med2.morning = req.body.morningCheckbox2;
+  det.med2.noon = req.body.noonCheckbox2;
+  det.med2.night = req.body.nightCheckbox2;
+  det.med3.morning = req.body.morningCheckbox3;
+  det.med3.noon = req.body.noonCheckbox3;
+  det.med3.night = req.body.nightCheckbox3;
+
+  var insertDocument = function(db, callback) {
+     db.collection('user').insertOne(det, function(err, result) {
+      assert.equal(err, null);
+      console.log(det);
+      console.log("Inserted a document into the restaurants collection.");
+      callback();
+    });
+  };
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    insertDocument(db, function() {
+        db.close();
+    });
+  });
+
+};
 
 /**
  * - POST : set prescription
